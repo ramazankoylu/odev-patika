2d6f337b3423a8426a760bf45404c42c3455b74e
Ramazan Köylü <ramazankoylu4040@gmail.com>
2023-03-07 20:53:00 +0300

    patika odevi yapildi

---
+++ src/App.js
@@ -0,0 +1,21 @@
+const { default: axios } = require("axios");
+
+async function getData(Number) {
+
+  const getUsers = async (Number) => {
+    const { data } = await axios("https://jsonplaceholder.typicode.com/users/" + Number);
+    console.log(data);
+  }
+
+
+  const getPost = async (Number) => {
+    const { data } = await axios("https://jsonplaceholder.typicode.com/posts?id=" + Number);
+    console.log(data);
+  }
+  const userId = await getUsers(Number)
+  const postUser = await getPost(Number)
+  return userId + postUser
+}
+
+
+export default getData;
